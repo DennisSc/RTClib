27f74cfff76a9deca33fe1d46798ab85c9de71d5
MrAlvinTesting <mralvin@3iii.dk>
2016-01-08 14:53:45 +0100

    implemented DS3231 class and functions

--- README.md
+++ README.md
@@ -5,34 +5,36 @@ For details on using this library with an RTC module like the DS1307, see the gu
 To download. click the DOWNLOADS button to the right, and rename the uncompressed folder RTClib.
 
 Place the RTClib folder in your *arduinosketchfolder*/libraries/ folder. 
-You may need to create the libraries subfolder if its your first library. Restart the IDE.
+You may need to create the libraries subfolder if its your first library. 
+Restart the IDE.
 
-<!-- START COMPATIBILITY TABLE -->
 
-## Compatibility
+Basic functions  (see libraries/examples of how to use):
+  .begin
+  .adjust
+  .isrunning
+  .now
+  
+DS1307 specific functions:
+  .readnvram
+  .writenvram
+  .readSqwPinMode
+  .writeSqwPinMode
 
-MCU               | Tested Works | Doesn't Work | Not Tested  | Notes
------------------ | :----------: | :----------: | :---------: | -----
-Atmega328 @ 16MHz |      X       |             |            | 
-Atmega328 @ 12MHz |      X       |             |            | 
-Atmega32u4 @ 16MHz |      X       |             |            | Use SDA/SCL on pins D3 &amp; D2
-Atmega32u4 @ 8MHz |      X       |             |            | Use SDA/SCL on pins D3 &amp; D2
-ESP8266           |      X       |             |            | SDA/SCL default to pins 4 &amp; 5 but any two pins can be assigned as SDA/SCL using Wire.begin(SDA,SCL)
-Atmega2560 @ 16MHz |      X       |             |            | Use SDA/SCL on Pins 20 &amp; 21
-ATSAM3X8E         |      X       |             |            | Use SDA1 and SCL1
-ATSAM21D          |      X       |             |            | 
-ATtiny85 @ 16MHz  |             |             |     X       | 
-ATtiny85 @ 8MHz   |             |             |     X       | 
+DS3231 specific functions:
+  .getTemperature
+  .getA1Time
+  .setA1Time
+  .setAlarm1Simple
+  .turnOnAlarm
+  .turnOffAlarm
+  .checkAlarmEnabled
+  .checkIfAlarm
+  
+DS3231 functionality tested on Arduino Pro Mini (Jan 8, 2016 - MrAlvin)
+For other compatibility details see https://github.com/adafruit/RTClib
 
-  * ATmega328 @ 16MHz : Arduino UNO, Adafruit Pro Trinket 5V, Adafruit Metro 328, Adafruit Metro Mini
-  * ATmega328 @ 12MHz : Adafruit Pro Trinket 3V
-  * ATmega32u4 @ 16MHz : Arduino Leonardo, Arduino Micro, Arduino Yun, Teensy 2.0
-  * ATmega32u4 @ 8MHz : Adafruit Flora, Bluefruit Micro
-  * ESP8266 : Adafruit Huzzah
-  * ATmega2560 @ 16MHz : Arduino Mega
-  * ATSAM3X8E : Arduino Due
-  * ATSAM21D : Arduino Zero, M0 Pro
-  * ATtiny85 @ 16MHz : Adafruit Trinket 5V
-  * ATtiny85 @ 8MHz : Adafruit Gemma, Arduino Gemma, Adafruit Trinket 3V
+ToDo: 
+ - debug Alarm2
+ - make DS3231 SQW functon calls similar to DS1307 function calls
 
-<!-- END COMPATIBILITY TABLE -->
--- RTClib.h
+++ RTClib.h
@@ -1,6 +1,10 @@
 // Code by JeeLabs http://news.jeelabs.org/code/
 // Released to the public domain! Enjoy!
 
+// Merged DS3231 functions from: github/coobro/RTClib  by  MrAlvin 2012-02-27
+// Alarm code for DS3231 heavily used/modified from Eric Ayars DS3231 library  by  Coobro
+// Eric Ayars code is located at: http://hacks.ayars.org/2011/04/ds3231-real-time-clock.html
+
 #ifndef _RTCLIB_H_
 #define _RTCLIB_H_
 
@@ -98,5 +102,91 @@ public:
 protected:
     static long offset;
 };
+
+// RTC based on the DS3231 chip connected via I2C
+class RTC_DS3231
+{
+public:
+    uint8_t begin(void);
+    void adjust(const DateTime& dt);
+    uint8_t isrunning(void);
+    DateTime now();
+
+    // Temperature function
+
+    float getTemperature();     
+
+    void getA1Time(byte& A1Day, byte& A1Hour, byte& A1Minute, byte& A1Second, byte& AlarmBits, bool& A1Dy, bool& A1h12, bool& A1PM); 
+/* Retrieves everything you could want to know about alarm
+ * one. 
+ * A1Dy true makes the alarm go on A1Day = Day of Week,
+ * A1Dy false makes the alarm go on A1Day = Date of month.
+ *
+ * byte AlarmBits sets the behavior of the alarms:
+ *	Dy	A1M4	A1M3	A1M2	A1M1	Rate
+ *	X	1		1		1		1		Once per second
+ *	X	1		1		1		0		Alarm when seconds match
+ *	X	1		1		0		0		Alarm when min, sec match
+ *	X	1		0		0		0		Alarm when hour, min, sec match
+ *	0	0		0		0		0		Alarm when date, h, m, s match
+ *	1	0		0		0		0		Alarm when DoW, h, m, s match
+ *
+ *	Dy	A2M4	A2M3	A2M2	Rate
+ *	X	1		1		1		Once per minute (at seconds = 00)
+ *	X	1		1		0		Alarm when minutes match
+ *	X	1		0		0		Alarm when hours and minutes match
+ *	0	0		0		0		Alarm when date, hour, min match
+ *	1	0		0		0		Alarm when DoW, hour, min match
+ */	
+    void getA2Time(byte& A2Day, byte& A2Hour, byte& A2Minute, byte& AlarmBits, bool& A2Dy, bool& A2h12, bool& A2PM); 
+        // Same as getA1Time();, but A2 only goes on seconds == 00.
+    void setA1Time(byte A1Day, byte A1Hour, byte A1Minute, byte A1Second, byte AlarmBits, bool A1Dy, bool A1h12, bool A1PM); 
+        // Set the details for Alarm 1	
+    void setAlarm1Simple(byte hour, byte minute);
+        // A simple hour/minute alarm.
+    void setA2Time(byte A2Day, byte A2Hour, byte A2Minute, byte AlarmBits, bool A2Dy, bool A2h12, bool A2PM); 
+        // Set the details for Alarm 2
+    void setAlarm2Simple(byte hour, byte minute);
+        // A simple hour/minute alarm.	
+    void turnOnAlarm(byte Alarm); 
+        // Enables alarm 1 or 2 and the external interrupt pin.
+        // If Alarm != 1, it assumes Alarm == 2.
+    void turnOffAlarm(byte Alarm); 
+        // Disables alarm 1 or 2 (default is 2 if Alarm != 1);
+        // and leaves the interrupt pin alone.
+    bool checkAlarmEnabled(byte Alarm); 
+        // Returns T/F to indicate whether the requested alarm is
+        // enabled. Defaults to 2 if Alarm != 1.
+    bool checkIfAlarm(byte Alarm); 
+        // Checks whether the indicated alarm (1 or 2, 2 default);
+        // has been activated.
+
+    // Oscillator functions
+
+    void enableOscillator(bool TF, bool battery, byte frequency); 
+        // turns oscillator on or off. True is on, false is off.
+        // if battery is true, turns on even for battery-only operation,
+        // otherwise turns off if Vcc is off.
+        // frequency must be 0, 1, 2, or 3.
+        // 0 = 1 Hz
+        // 1 = 1.024 kHz
+        // 2 = 4.096 kHz
+        // 3 = 8.192 kHz (Default if frequency byte is out of range);
+    void enable32kHz(bool TF); 
+		// Turns the 32kHz output pin on (true); or off (false).
+    bool oscillatorCheck();;
+        // Checks the status of the OSF (Oscillator Stop Flag);.
+        // If this returns false, then the clock is probably not
+        // giving you the correct time.
+        // The OSF is cleared by function setSecond();.		
+
+private:
+    byte readControlByte(bool which); 
+		// Read selected control byte: (0); reads 0x0e, (1) reads 0x0f
+    void writeControlByte(byte control, bool which); 
+		// Write the selected control byte. 
+		// which == false -> 0x0e, true->0x0f.    
+};
+
 
 #endif // _RTCLIB_H_
